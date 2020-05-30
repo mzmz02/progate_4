a3a5570a088443296cd973c3949b29a872626eb3
mzmz__02 <bb.mai.k0702@gmail.com>
2020-05-30 20:19:27 +0900

    レッスン一覧を作る(カーソルがあうと表示する)

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-$(function(){
-  
-  $('.signup-show').click(function(){
-    $('#signup-modal').fadeIn();
+$(function() {
+
+    // 新規登録モーダル
     
+    $('.signup-show').click(function() {
+      $('#signup-modal').fadeIn();
+    });
+  
+    $('#close-modal').click(function() {
+      $('#signup-modal').fadeOut();
+    });
+    
+    // 言語一覧
+    
+    $('.lesson').hover(
+      function(){
+        $(this).find('.text-contents').addClass('text-active');
+      },
+      function(){
+        $(this).find('.text-contents').removeClass('text-active');
+      }
+    )
+    
+  
   });
   
-  $('#close-modal').click(function(){
-    $('#signup-modal').fadeOut();
-  });
-  
-  
-});
