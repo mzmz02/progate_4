7aa0a5e007f6c1533b21575cc31498bffab017b9
mzmz__02 <bb.mai.k0702@gmail.com>
2020-05-30 20:46:32 +0900

    FAQ部分でアコーディオンの作成

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(function() {
 
     // 新規登録モーダル
-    
+  
     $('.signup-show').click(function() {
       $('#signup-modal').fadeIn();
     });
@@ -9,18 +9,33 @@ $(function() {
     $('#close-modal').click(function() {
       $('#signup-modal').fadeOut();
     });
-    
+  
     // 言語一覧
-    
+  
     $('.lesson').hover(
-      function(){
+      function() {
         $(this).find('.text-contents').addClass('text-active');
       },
-      function(){
+      function() {
         $(this).find('.text-contents').removeClass('text-active');
       }
-    )
+    );
     
+    $('.faq-list-item').click(function(){
+      
+      if($('.answer').hasClass('open')){
+        $(this).find('.answer').removeClass('open');
+        $(this).find('.answer').slideUp();
+        $(this).find('span').text('+');
+      }
+      else{
+        $(this).find('.answer').addClass('open');
+        $(this).find('.answer').slideDown();
+        $(this).find('span').text('-');
+      }
+    })
+    
+  
   
   });
   
